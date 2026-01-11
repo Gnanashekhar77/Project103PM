092a35d2b7e522f628944c14dc227c2b0caccbab
Gnanashekhar77 <gnanashekharm7@gmail.com>
2026-01-11 19:55:46 +0530

    First time push

---
+++ src/test/java/com/sample/Sample.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import java.lang.reflect.Method;
+
+public class Sample {
+	
+	private void mathod1() {
+
+		System.out.println("1");
+	}
+	
+	private void method2() {
+
+		System.out.println("2");
+	}
+	
+	private void method3() {
+
+		System.out.println("3");
+	}
+
+}
